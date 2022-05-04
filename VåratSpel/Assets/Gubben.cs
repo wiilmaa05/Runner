f72c2d821d79fd1dc5e1234780960a01f0a81acd
Pernilla30 <104619394+Pernilla30@users.noreply.github.com>
2022-05-04 09:59:19 +0200

    Player movment
    Spelarens rörelse

--- VåratSpel/Assets/Gubben.cs
+++ VåratSpel/Assets/Gubben.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Gubben : MonoBehaviour
 {
+    //Hur högt man ska hoppa och hastigheten
     [SerializeField, Range(1, 10)]
-    float speed;
+    public float speed;
+    public float Jumb;
+
+    //Variablet för att player ska gå fram
+    public Vector3 userDirection = Vector3.right;
+    public static int movespeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +22,22 @@ public class Gubben : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("d"))
+        //Koden för objektet att gå runt t.ex fram, tillbaka, höger och vänster + rotation
+        if (Input.GetKey("d"))
         {
-            transform.position += new Vector3(0, speed, 0);
+            transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
         }
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+        }
+        // Hopp knappen för upp och ner
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, Jumb, 0) * Time.deltaTime;
+        }
+
+        //Player går fram konstant 
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
     }
 }
--- VåratSpel/Assets/Kamera.cs
+++ VåratSpel/Assets/Kamera.cs
@@ -17,7 +17,7 @@ public class Kamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //För att kameran ska följa med objectet 
         transform.position = target.position + offset;
